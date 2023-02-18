c5e85bd1b4b52dd494a5643f164f81aa4acb6141
JB <jeanbaptiste.foncin@gmail.com>
2023-02-18 11:06:56 +0100

    finished layers and sublayers

---
+++ modeling/layers.py
@@ -0,0 +1,96 @@
+from torch import nn
+
+from .sublayers import BottleNeck, ResidualMultiHeadAttention
+
+
+class BoardEncoderLayer(nn.Module):
+    """
+    basic transformer encoder layer
+    """
+    def __init__(self, 
+                 embedding_dim,
+                 nb_head,
+                 dim_per_head,
+                 bottleneck_hidden_dim):
+        """
+        Args:
+            embedding_dim (int): embedding size
+            nb_head (int): number of attention heads
+            dim_per_head (int): hidden size per head
+            bottleneck_intermediate_dim (int): intermediate size in bottleneck
+        """
+        super().__init__()
+        
+        self.multihead_attention = ResidualMultiHeadAttention(nb_head, dim_per_head, embedding_dim)
+        
+        self.bottleneck = BottleNeck(embedding_dim, bottleneck_hidden_dim)
+        
+        self.layernorm_1 = nn.LayerNorm(embedding_dim)
+        self.layernorm_2 = nn.LayerNorm(embedding_dim)
+
+        
+    def forward(self, hidden_state):
+        """
+        Args:
+            hidden_state (t.tensor): tensor of size (seq_len, embedding_dim)
+
+        Returns:
+            t.tensor: tensor of size (seq_len, embedding_dim)
+        """        
+        hidden_state = self.layernorm_1(hidden_state)
+        
+        hidden_state = self.multihead_attention(hidden_state, hidden_state, hidden_state)
+        
+        hidden_state = self.layernorm_2(hidden_state)
+        
+        hidden_state = self.bottleneck(hidden_state)
+        
+        return hidden_state
+    
+
+class ActionDecoderLayer(nn.Module):
+    """
+    transformer decoder to score all possible actions
+    """
+    def __init__(self ,
+                 embedding_dim,
+                 nb_head,
+                 dim_per_head,
+                 bottleneck_hidden_dim):
+        """
+        Args:
+            embedding_dim (int): embedding size
+            nb_head (int): number of attention heads
+            dim_per_head (int): hidden size per head
+            bottleneck_intermediate_dim (int): intermediate size in bottleneck
+        """
+        super().__init__()
+        
+        self.multihead_attention = ResidualMultiHeadAttention(nb_head, dim_per_head, embedding_dim)
+        
+        self.bottleneck = BottleNeck(embedding_dim, bottleneck_hidden_dim)
+        
+        self.layernorm_1 = nn.LayerNorm(embedding_dim)
+        self.layernorm_2 = nn.LayerNorm(embedding_dim)
+        
+    def forward(self, enc_output, decoder_hidden_state):
+        """
+        Args:
+            enc_output (torch.tensor): tensor of size (number of pieces * embedding_dim)
+            decoder_hidden_state (torch.tensor): tensor of size (number of possible moves * embedding dim)
+        Returns:
+            torch.tensor: tensor of size ?
+        """        
+        decoder_hidden_state = self.layernorm_1(decoder_hidden_state)
+        
+        decoder_hidden_state = self.multihead_attention(decoder_hidden_state,
+                                                        enc_output,
+                                                        enc_output)
+        
+        decoder_hidden_state = self.layernorm_2(decoder_hidden_state)
+        
+        decoder_hidden_state = self.bottleneck(decoder_hidden_state)
+        
+        return decoder_hidden_state
+        
+        
---
+++ modeling/sublayers.py
@@ -0,0 +1,140 @@
+from math import sqrt
+
+from torch import nn
+
+
+class ChessFormerEncoderEmbedding(nn.Module):
+    """
+    basic embedding layer
+    """
+    
+    def __init__(self, embedding_dim):
+        """
+        Args:
+            embedding_dim (int): size of the embeddings
+        """
+        
+        super().__init__()
+        
+        self.position_emb = nn.Embedding(64, embedding_dim=embedding_dim)
+        self.piece_emb = nn.Embedding(6, embedding_dim=embedding_dim)
+        self.color_emb = nn.Embedding(2, embedding_dim=embedding_dim)
+        
+    def forward(self, pieces_ids, color_ids, indexes):
+        """
+        Args:
+            pieces_ids (t.tensor): id of each piece
+            color_ids (t.tensor): color of each piece (0 or 1)
+            indexes (t.tensor): piece location on the board (0 to 63)
+        """
+        return self.position_emb(indexes) + self.piece_emb(pieces_ids) + self.color_emb(color_ids)
+    
+    
+class ChessFormerDecoderEmbedding(nn.Module):
+    """
+    positional embeddings for decoder
+    """
+    def __init__(self, embedding_dim):
+        """
+        Args:
+            embedding_dim (int): embedding size
+        """
+        super().__init__()
+        
+        self.initial_position_embedding = nn.Embedding(64, embedding_dim=embedding_dim)
+        self.destination_embedding = nn.Embedding(64, embedding_dim=embedding_dim)
+    
+    def forward(self, initial_position_indexes, destination_indexes):
+        """
+        Args:
+            initial_position_indexes (t.tensor): tensor of starting point of all allowed movements
+            destination_indexes (_type_): destination for all allowed moves
+
+        Returns:
+            t.tensor: tensor of dim (number of allowed moves * embedding_dim)
+        """      
+        return self.initial_position_embedding(initial_position_indexes) + \
+               self.destination_embedding(destination_indexes)
+    
+class ResidualMultiHeadAttention(nn.Module):
+    """
+    simple attention head, basic implementation. 
+    WARNING: THE INPUT HAS TO BE NORMALIZED BEFORE
+    """
+    def __init__(self, nb_head, dim_per_head, embedding_dim):
+        """
+        Args:
+            nb_head (int): number of attention heads
+            dim_per_head (_type_): hidden size of each head
+            embedding_dim (_type_): hidden size of the model
+        """
+        super().__init__()
+        
+        self.nb_head = nb_head
+        self.dim_per_head = dim_per_head
+        self.embedding_dim = embedding_dim
+        self.query_projector = nn.Linear(embedding_dim, dim_per_head * nb_head, bias=False)
+        self.key_projector = nn.Linear(embedding_dim, dim_per_head * nb_head, bias=False)
+        self.value_projector = nn.Linear(embedding_dim, dim_per_head * nb_head, bias=False)
+        self.output = nn.Linear(dim_per_head * nb_head, embedding_dim, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        
+    def forward(self, hidden_state_query, hidden_state_key, hidden_state_value):
+        """
+        Args:
+            hidden_state_query (t.tensor): the hidden state to be projected as query
+            hidden_state_key (t.tensor): the hidden state to be projected as key
+            hidden_state_value (t.tensor): the hidden state to be projected as value
+
+        Returns:
+            t.tensor: hidden state for the next layer
+        """
+        query = self.query_projector(hidden_state_query) 
+        key = self.key_projector(hidden_state_key)
+        value = self.value_projector(hidden_state_value)
+        
+        seq_len_q = query.size(0)
+        seq_len_k = key.size(0)
+        
+        query  = query.view(seq_len_q, self.nb_head, self.dim_per_head).transpose(0, 1)
+        key = key.view(seq_len_k, self.nb_head, self.dim_per_head).transpose(0, 1)
+        value = value.view(seq_len_k, self.nb_head, self.dim_per_head).transpose(0, 1)
+        attn_scores = query @ key.transpose(-1, -2) / sqrt(self.dim_per_head)
+        attn = self.softmax(attn_scores)
+        attn_product = attn @ value
+        attn_product = attn_product.transpose(0, 1).contiguous().view(seq_len_q, -1)
+        attn_applied = self.output(attn_product)
+        
+        return attn_applied + hidden_state_query
+    
+    
+class BottleNeck(nn.Module):
+    """
+    basic bottleneck module
+    """
+    def __init__(self, embedding_dim, hidden_dim):
+        """
+        Args:
+            embedding_dim (int): hidden size of the model
+            hidden_dim (int): internal dim of the bottleneck
+        """
+        super().__init__()
+        
+        self.w_1 = nn.Linear(embedding_dim, hidden_dim) 
+        self.w_2 = nn.Linear(hidden_dim, embedding_dim) 
+        self.activation = nn.GELU()
+
+    def forward(self, input_):
+        """
+        Args:
+            input_ (t.tensor): hidden state of the model, size (seq_len, emb_dim)
+
+        Returns:
+            t.tensor: input processed
+        """
+        out = self.w_2(self.activation(self.w_1(input_)))
+        
+        return out + input_     
+        
+        
+        
