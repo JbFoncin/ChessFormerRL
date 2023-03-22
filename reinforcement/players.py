f5e75137f51a606ff9004573fd69a60b646b552e
JB <jeanbaptiste.foncin@gmail.com>
2023-03-22 17:41:16 +0000

    improved linting

--- reinforcement/players.py
+++ reinforcement/players.py
@@ -117,9 +117,9 @@ class ModelPlayer(PlayerABC):
         actions_scores = self.model(**inference_data)
 
         actions_scores = actions_scores.squeeze(1)
-        
+
         chosen_action_value, chosen_action_index = actions_scores.cpu().topk(1)
-        
+
         output = PlayerOutput(action=possible_actions[chosen_action_index.item()],
                               action_index=chosen_action_index.item(),
                               estimated_action_value=chosen_action_value.item(),
