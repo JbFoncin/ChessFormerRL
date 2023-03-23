b00599533f6e797a709e6c6ba91f3d19de3986b6
JB <jeanbaptiste.foncin@gmail.com>
2023-03-23 17:45:39 +0000

    created DoubleModelPlayer class

--- reinforcement/players.py
+++ reinforcement/players.py
@@ -14,8 +14,10 @@ PlayerOutput = namedtuple('PlayerOutput',
                           field_names=['action',
                                        'action_index',
                                        'inference_data',
-                                       'estimated_action_value'],
+                                       'estimated_action_value',
+                                       'model_used'],
                           defaults=[None,
+                                    None,
                                     None,
                                     None,
                                     None])
@@ -114,7 +116,9 @@ class ModelPlayer(PlayerABC):
 
         inference_data = prepare_for_model_inference(board, self.color_map, self.model_device)
 
+        self.model.eval()
         actions_scores = self.model(**inference_data)
+        self.model.train()
 
         actions_scores = actions_scores.squeeze(1)
 
@@ -127,6 +131,7 @@ class ModelPlayer(PlayerABC):
 
         return output
 
+    @t.no_grad()
     def choose_random_action(self, board):
         """
         Args:
@@ -139,6 +144,96 @@ class ModelPlayer(PlayerABC):
         """
         action, chosen_action_index, _ = super().choose_random_action(board)
 
-        inference_data = prepare_for_model_inference(board, self.color_map)
+        inference_data = prepare_for_model_inference(board, self.color_map, self.model_device)
+
+        self.model.eval()
+        actions_scores = self.model(**inference_data)
+        self.model.train()
+
+        output = PlayerOutput(action=action,
+                              action_index=chosen_action_index,
+                              estimated_action_value=actions_scores[0, chosen_action_index].cpu().item(),
+                              inference_data=inference_data)
 
         return action, chosen_action_index, inference_data
+
+
+class DoubleModelPlayer(PlayerABC):
+    """
+    player using a two model as policy randomly
+    """
+    def __init__(self, model_1, model_2, random_action_rate, model_device):
+        """
+        Args:
+            color (str): 'b' for black or 'w' for white
+            model_1 (nn.Module): the first policy
+            model_1 (nn.Module): the second policy
+            random_action_rate (float): how often the player will act randomly
+            model_device (str): device of the models
+        """
+        super().__init__()
+
+        self.model_1 = model_1
+        self.model_2 = model_2
+        self.random_action_rate = random_action_rate
+        self.model_device = model_device
+        self.model = model_1
+        self.model_name = 'model_1'
+
+    @t.no_grad()
+    def choose_action(self, board):
+        """
+        Args:
+            board (chess.board): the current game object
+        Returns:
+            str: initital and final positions as str (like 'e2e4')
+            int: action index
+            inference data: dict of tensor to be stored in the replay buffer
+        """
+        if random() < self.random_action_rate:
+
+            return self.choose_random_action(board)
+
+        possible_actions = self.get_possible_actions(board)
+
+        inference_data = prepare_for_model_inference(board, self.color_map, self.model_device)
+
+        self.model.eval()
+        actions_scores = self.model(**inference_data)
+        self.model.train()
+
+        actions_scores = actions_scores.squeeze(1)
+
+        chosen_action_value, chosen_action_index = actions_scores.cpu().topk(1)
+
+        output = PlayerOutput(action=possible_actions[chosen_action_index.item()],
+                              action_index=chosen_action_index.item(),
+                              estimated_action_value=chosen_action_value.item(),
+                              inference_data=inference_data,
+                              model_used=self.model_name)
+
+        return output
+
+    def switch_models(self):
+        """
+        changes model
+        """
+        if self.model is self.model_1:
+            self.model = self.model_2
+            self.model_name = 'model_2'
+
+        else:
+            self.model = self.model_1
+            self.model_name = 'model_1'
+
+    def choose_random_action(self, board):
+        """
+        Args:
+            board (chess.Board): the current game
+
+        Returns:
+            str: next move as str
+            int: index of the chosen action
+            inference data: dict of tensor to be stored in the replay buffer
+        """
+        raise NotImplementedError('Not done yet')
