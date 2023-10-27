85503618161918b048e05f3576acd532ed4c4e23
JB <jeanbaptiste.foncin@gmail.com>
2023-10-27 17:15:48 +0000

    fixed indentation

--- reinforcement/dqn/dqn_v3.py
+++ reinforcement/dqn/dqn_v3.py
@@ -93,45 +93,111 @@ class DQNTrainerV3(DQNTrainerV2):
                                            'estimated_action_value': estimated_action_value})    
     
     
-        def train_batch(self):
-            """
-            samples and train one batch
+    def train_batch(self):
+        """
+        samples and train one batch
 
-            Returns:
-                float: loss value on the current batch
-            """
-            self.optimizer.zero_grad()
+        Returns:
+            float: loss value on the current batch
+        """
+        self.optimizer.zero_grad()
 
-            batch, sample_indexes, weights = self.make_training_batch()
+        batch, sample_indexes, weights = self.make_training_batch()
 
-            model_output = self.model(**batch['model_inputs'])
-            
-            gather_index = t.repeat_interleave(batch['targets']['targets_idx'],
-                                               self.agent.model.nb_quantiles)
-            
-            gather_index_reshaped = gather_index.view(self.batch_size, -1).unsqueeze(1)
+        model_output = self.model(**batch['model_inputs'])
+        
+        gather_index = t.repeat_interleave(batch['targets']['targets_idx'],
+                                            self.agent.model.nb_quantiles)
+        
+        gather_index_reshaped = gather_index.view(self.batch_size, -1).unsqueeze(1)
 
-            predicted = t.gather(model_output, dim=1,
-                                 index=gather_index_reshaped)
+        predicted = t.gather(model_output, dim=1,
+                                index=gather_index_reshaped)
 
-            loss_per_batch = self.loss(predicted.squeeze(-1), batch['targets']['targets'], weights)
+        loss_per_batch = self.loss(predicted, batch['targets']['targets'], weights)
 
-            loss_value = loss_per_batch.mean(0)
-            loss_value.backward()
+        loss_value = loss_per_batch.mean(0)
+        loss_value.backward()
 
-            new_sampling_scores = loss_per_batch.detach().cpu()
+        new_sampling_scores = loss_per_batch.detach().cpu()
 
-            self.optimizer.step()
+        self.optimizer.step()
 
-            for index, value in zip(sample_indexes, new_sampling_scores):
-                self.sampling_scores[index] = value.item()
+        for index, value in zip(sample_indexes, new_sampling_scores):
+            self.sampling_scores[index] = value.item()
 
-            model_state_dict = self.model.state_dict()
-            target_network_state_dict = self.target_network.state_dict()
+        model_state_dict = self.model.state_dict()
+        target_network_state_dict = self.target_network.state_dict()
 
-            for key in target_network_state_dict:
-                target_network_state_dict[key] = self.tau * model_state_dict[key] + (1 - self.tau) * target_network_state_dict[key]
+        for key in target_network_state_dict:
+            target_network_state_dict[key] = self.tau * model_state_dict[key] + (1 - self.tau) * target_network_state_dict[key]
 
-            self.target_network.load_state_dict(target_network_state_dict)
+        self.target_network.load_state_dict(target_network_state_dict)
 
-            return loss_value.cpu().detach().item()
+        return loss_value.cpu().detach().item()
+        
+    @t.no_grad()
+    def make_training_batch(self):
+        """
+        creates batch for training
+        
+
+        Returns:
+            dict[str, torch.Tensor]: model inputs and target
+            list[int]: indexes of sampled data in buffer. Used to update the
+                       prioritized buffer sampling probas.
+            np.array: weights to be used for batch training
+        """
+        batch_data_indexes, weights = self.sample_indexes()
+
+        batch_data_list = [self.buffer[i] for i in batch_data_indexes]
+
+        # When we separate the data needing update and the other we loose index position
+        # and therefore cannot update the priority buffer.
+
+        need_update, others = [], []
+        need_update_indexes, other_indexes = [], []
+        weights_update, weights_other = [], []
+
+        for index, data, weight in zip(batch_data_indexes,
+                                       batch_data_list,
+                                       weights):
+
+            if 'q_hat_input' in data:
+                need_update.append(data)
+                need_update_indexes.append(index)
+                weights_update.append(weight)
+
+            else:
+                others.append(data)
+                other_indexes.append(index)
+                weights_other.append(weight)
+
+        if need_update:
+
+            q_hat_batch = prepare_input_for_batch(need_update,
+                                                  device=self.model_device,
+                                                  with_target=False)
+
+            q_hat_output = self.target_network(**q_hat_batch['model_inputs']).detach()
+
+            q_hat_values = t.gather(q_hat_output,
+                                    dim=1,
+                                    index=q_hat_batch['targets']['targets_idx'].unsqueeze(1)).cpu()
+
+
+            for i, data in enumerate(need_update):
+                data['target'] = q_hat_values[i].item() + data['reward']
+
+        for data in others:
+            data['target'] = data['reward']
+
+        batch_data = need_update + others
+        data_indexes = need_update_indexes + other_indexes
+        weights = weights_update + weights_other
+
+        weights = t.tensor(weights, device=self.model_device)
+
+        batch = prepare_input_for_batch(batch_data, self.model_device)
+
+        return batch, data_indexes, weights
