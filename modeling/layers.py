1a9bb0f535484566cdb8edafb19c87f6a77491a8
JB <jeanbaptiste.foncin@gmail.com>
2023-11-26 17:14:53 +0000

    removed unused code

--- modeling/layers.py
+++ modeling/layers.py
@@ -34,36 +34,6 @@ class ChessFormerEncoderEmbedding(nn.Module):
         return self.position_emb(self.indexes) + self.piece_emb(pieces_ids) + self.color_emb(color_ids)
 
 
-class ChessFormerEncoderEmbeddingAdvantage(nn.Module):
-    """
-    Embedding layer for positional, color and piece encoding.
-    The input has a length of 65 instead of 64 because the first token
-    is an estimation of the state value.
-    """
-    def __init__(self, embedding_dim):
-        """
-        Args:
-            embedding_dim (int): size of the embeddings
-        """
-
-        super().__init__()
-
-        self.position_emb = nn.Embedding(64, embedding_dim=embedding_dim)
-        self.piece_emb = nn.Embedding(7, embedding_dim=embedding_dim)
-        self.color_emb = nn.Embedding(3, embedding_dim=embedding_dim)
-        self.register_buffer('indexes', t.tensor(BOARD_INDEXES_ADVANTAGE, dtype=t.long))
-
-    def forward(self, pieces_ids, color_ids):
-        """
-        Args:
-            pieces_ids (torch.tensor): id of each piece
-            color_ids (torch.tensor): color of each piece (0 for empty box, 1 for player piece, 2 for competitor piece)
-        Returns:
-            torch.tensor: tensor of dim (64 * embedding_dim)
-        """
-        return self.position_emb(self.indexes) + self.piece_emb(pieces_ids) + self.color_emb(color_ids)
-
-
 class ChessFormerDecoderEmbedding(nn.Module):
     """
     positional embeddings for decoder
